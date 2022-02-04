92a95c290b323d97a4f64a5a247e7895e4aa64c2
Zacharia Njue <njuezachary@gmail.com>
2022-02-04 11:41:35 +0300

    revamped the code to use the try and except

--- Search_data.py
+++ Search_data.py
@@ -1,16 +1,17 @@
 import csv
-from unittest import case
 
-req_file = '/Users/zachnjue/Documents/Scripting/Programming Assignment/ProgrammingAssignment.csv'
-string_to_search_for = input(
-    "Please search data using either of these string: (1) - Zio code, (2) - State, (3) - City, (4) - Bank, (5) - Type:  ")
+path_to_csv_file = '/Users/zachnjue/Documents/Scripting/Programming Assignment/ProgrammingAssignment.csv'
 
-file_open = open(req_file, 'r')
-data = csv.reader(file_open)
+try:
+    string_to_search_for = input(
+        "Please search your data using the options listed below : (1) - Zio code, (2) - State, (3) - City, (4) - Bank, (5) - Type:, (6) - Zip code:   ")
 
+    file_open = open(path_to_csv_file, 'r')
+    data = csv.reader(file_open)
 
-for each_entry in data:
-    if string_to_search_for in each_entry:
-        print(each_entry)
-
-file_open.close()
+    for each_entry in data:
+        if string_to_search_for in each_entry:
+            print(each_entry)
+    file_open.close()
+except Exception as e:
+    print(e)
