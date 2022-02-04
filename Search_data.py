995e369bacaee6157ffee2fe7034bfecfda4e5fb
Zacharia Njue <njuezachary@gmail.com>
2022-02-04 11:46:08 +0300

    formatted the output of the options to be listed

--- Search_data.py
+++ Search_data.py
@@ -4,7 +4,7 @@ path_to_csv_file = '/Users/zachnjue/Documents/Scripting/Programming Assignment/P
 
 try:
     string_to_search_for = input(
-        "Please search your data using the options listed below : (1) - Zio code, (2) - State, (3) - City, (4) - Bank, (5) - Type:, (6) - Zip code:   ")
+        "Please search your data using the options listed below:\n(1) - Zip code,\n(2) - State,\n(3) - City,\n(4) - Bank,\n(5) - Type:,\n(6) - Zip code:\n   ")
 
     file_open = open(path_to_csv_file, 'r')
     data = csv.reader(file_open)
