d2471586b589bd64b3d742e8c6abeb872e470772
Zacharia Njue <njuezachary@gmail.com>
2022-02-04 11:28:59 +0300

    refined the search input option for the code

--- Search_data.py
+++ Search_data.py
@@ -1,14 +1,16 @@
 import csv
+from unittest import case
 
 req_file = '/Users/zachnjue/Documents/Scripting/Programming Assignment/ProgrammingAssignment.csv'
-stringr
-string = 'Dallas'
+string_to_search_for = input(
+    "Please search data using either of these string: (1) - Zio code, (2) - State, (3) - City, (4) - Bank, (5) - Type:  ")
 
 file_open = open(req_file, 'r')
 data = csv.reader(file_open)
 
 
 for each_entry in data:
-    if string in each_entry:
+    if string_to_search_for in each_entry:
         print(each_entry)
+
 file_open.close()
